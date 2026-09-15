80842de897ab5eddfece06047a20e30d1dc75381
Your Name <phantomy084@gmail.com>
2026-09-15 08:05:11 +0500

    new file for numbers

---
+++ Practice1/python-basics/numbers.py
@@ -0,0 +1,21 @@
+#int numbers
+n=5
+print(type(n))
+
+#float numbers
+f=24.5
+print(type(f))
+
+#complex numbers
+c=2j
+print(type(c))
+
+#convert numbers
+m=21.3
+m1=int(m)
+print(m,m1)
+
+#random number
+import random
+
+print(random.randrange(1,10))
