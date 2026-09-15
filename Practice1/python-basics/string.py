2ab0a150c51376a40cfa8b0b250527fdc79f55cb
Your Name <phantomy084@gmail.com>
2026-09-15 08:19:40 +0500

    ff

---
+++ Practice1/python-basics/string.py
@@ -0,0 +1,24 @@
+#ex1 
+print("hello")
+
+#ex2 slicing 
+word = "Kokshetau"
+print(word[:5])
+
+#ex3 upper or lower
+a="apple"
+print(a.upper())
+b="HaT"
+print(b.lower())
+
+#ex4
+n="apple"
+m="banana"
+x=n+m
+print(x)
+
+#ex5 f-stirng
+age = 21
+txt=f"I'm {age} years old."
+print(txt)
+
