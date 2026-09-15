5c3070ae976fba8fe8db4acb4cb78cf0518c16c2
Your Name <phantomy084@gmail.com>
2026-09-15 07:59:14 +0500

    new file for datatype

---
+++ Practice1/python-basics/data_type.py
@@ -0,0 +1,19 @@
+#integer type
+x=int(20)
+print(x)
+
+#string type
+m=str("Kbtu")
+print(m)
+
+#float type 
+f=float(20.5)
+print(f)
+
+#list type
+n=list(("cap", "hat", "cup"))
+print(n[1])
+
+#how to know which type is
+a=5
+print(type(x))
